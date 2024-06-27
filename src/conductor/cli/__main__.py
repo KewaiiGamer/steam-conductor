cfbd7eaa3b428106522444f68c82f79a43c25eb1
Carlos Chulo <cchulo.dev@gmail.com>
2024-06-26 20:00:08 -0700

    moved the commands to their own list and strongly typed them

--- src/conductor/cli/__main__.py
+++ src/conductor/cli/__main__.py
@@ -2,6 +2,9 @@
 
 import sys
 import argparse
+
+from conductor.lib.command import BaseCommand
+
 from commands import info, add_shortcut, remove_shortcut, install
 import importlib.metadata
 
@@ -13,14 +16,15 @@ def main():
     parser = argparse.ArgumentParser(description="All in one steam game manager")
     parser.add_argument('--version', action='store_true', help='Print the version of conductor')
     subparsers = parser.add_subparsers(metavar="[command]", dest="command", help="Available commands")
+    commands: list[BaseCommand] = [
+        info.Command(),
+        add_shortcut.Command(),
+        install.Command(),
+        remove_shortcut.Command(),
+    ]
 
     command_lookup = {}
-    for command in [
-            info.Command(),
-            add_shortcut.Command(),
-            install.Command(),
-            remove_shortcut.Command(),
-    ]:
+    for command in commands:
         command.register_options(subparsers)
         command_lookup[command.get_command_str()] = command
 
