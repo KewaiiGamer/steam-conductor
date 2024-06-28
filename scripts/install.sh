05048b79f9fef45c0e7c59ac58f5e5b1de4dfa29
Carlos Chulo <cchulo.dev@gmail.com>
2024-06-27 20:58:09 -0700

    updated the install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,6 +2,16 @@
 
 set -euo pipefail
 
+if [[ ! -d ./src ]]; then
+  echo "this script needs to be called from the root of the project"
+  exit 1
+fi
+
+if [[ ! -d ./venv ]]; then
+  python -m venv venv
+  source venv/bin/activate
+fi
+
 pip3 install -e .
 pyinstaller --onefile --clean --name conductor-cli src/conductor/cli/__main__.py --dist bin
 mkdir -p ~/.local/bin
